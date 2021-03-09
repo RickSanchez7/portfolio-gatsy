cf2603dc27bf0694800e8e09fd8f3381ebfeac3a
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2021-03-09 22:55:59 +0000

    added transition to contact button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,41 @@
-import React from "react";
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import React, { useState, useEffect } from "react";
 import Image from "gatsby-image";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
+
+import { HiOutlineMail } from "react-icons/hi";
+
 import SocialLinks from "../constants/socialLinks";
 
 const Hero = () => {
+  const [transform, setTransform] = useState(false);
+
   const {
     file: {
       childImageSharp: { fluid },
     },
   } = useStaticQuery(query);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const scrollCheck = 300;
+      if (window.scrollY > scrollCheck) {
+        setTransform(true);
+      } else {
+        setTransform(false);
+      }
+    };
+
+    // setting the event handler from web API
+    document.addEventListener("scroll", onScroll);
+
+    // cleaning up from the web API
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  });
+
+  const add = transform ? "btn-transform" : "";
+
   return (
     <header className="hero">
       <div className="section-center hero-center">
@@ -18,9 +44,11 @@ const Hero = () => {
             <div className="underline"></div>
             <h1>i'm ricardo</h1>
             <h4>web Developer</h4>
-            <AniLink fade to="/contact" className="btn">
-              contact me
-            </AniLink>
+            <div className="contact-me-container">
+              <Link to="/contact" className={`btn contact-me ${add}`}>
+                {transform ? <HiOutlineMail size={50} /> : "contact me"}
+              </Link>
+            </div>
             <SocialLinks />
           </div>
         </article>
