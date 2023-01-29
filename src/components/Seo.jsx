97f0c25db453b6859cb33524c0407bade2f52050
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2023-01-29 22:51:19 +0000

    add twitter card info

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -30,12 +30,20 @@ export const SEO = ({ title, description, image, article, lang }) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {(article ? true : null) && (
+        <meta property="og:type" content="portfolio" />
+      )}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
         <meta property="og:description" content={seo.description} />
       )}
       {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:site" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:creator" content={seo.title} />
     </Helmet>
   );
 };
