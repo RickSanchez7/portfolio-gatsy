85eb9ee8a432157774775f288bf2c2cff1001bfb
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2023-01-29 20:31:47 +0000

    preconnect

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -9,16 +9,20 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossorigin
+        />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           rel="preload"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           as="style"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600&display=swap"
         />
       </Helmet>
       {children}
